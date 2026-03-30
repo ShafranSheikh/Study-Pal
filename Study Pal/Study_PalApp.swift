1a61e9f73a07ae2cfada9dc64f1363126358be8f
ShafranSheikh <shafransheikh@gmail.com>
2026-03-30 08:12:41 +0530

    Initial Commit

---
+++ Study Pal/Study_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_PalApp.swift
+//  Study Pal
+//
+//  Created by Mohamed Shafran on 2026-03-30.
+//
+
+import SwiftUI
+
+@main
+struct Study_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
